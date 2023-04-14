f6de4b0e07ceb5fad050db910feda285ede4b05b
Pranav1326 <visavadiapa@gmail.com>
2023-04-14 16:05:10 +0530

    Admin Dashboard

--- src/components/Admin/AdminDashboard/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard/AdminDashboard.jsx
@@ -3,11 +3,62 @@ import './admindashboard.css';
 import AdminProfileCard from './AdminProfileCard';
 import FdHistory from '../../User/Profile/FdHistory';
 import CreateRate from '../CreateRate/CreateRate';
+import CurrentRates from './CurrentRates';
 
 const AdminDashboard = () => {
     const [ fdBtn, setfdBtn ] = useState(false);
     const [ accountBtn, setAccountBtn ] = useState(false);
-    const [ btn, setBtn ] = useState("fd");
+    const [ btn, setBtn ] = useState("currentRate");
+    
+    const renderDashboard = (btn) => {
+        if(btn === "fd"){
+            return(
+                <div className="fd-history">
+                    <h1 className='heading'> FD History </h1>
+                    <FdHistory
+                        user="Raj"
+                        transaction={"created fd"}
+                        createdAt={new Date()}
+                        amount={5000}
+                        interest={3}
+                        months={6}
+                    />
+                    <FdHistory
+                        user="Preyansh"
+                        transaction={"matured fd"}
+                        createdAt={new Date()}
+                        amount={5000}
+                        interest={2.4}
+                        months={3}
+                    />
+                    <FdHistory
+                        user="Rishi"
+                        transaction={"created fd"}
+                        createdAt={new Date()}
+                        amount={10000}
+                        interest={4.7}
+                        months={18}
+                    />
+                </div>
+            );
+        }
+        else if(btn === "createRate"){
+            return <CreateRate />
+        }
+        else if(btn === "currentRate"){
+            return (
+                <div className='current-rates-main'>
+                    <h1 className='heading'>Current Rates</h1>
+                    <div className="current-rates-div">
+                        <CurrentRates for="student" />
+                        <CurrentRates for="normal" />
+                        <CurrentRates for="senior" />
+                    </div>
+                </div>
+            );
+        }
+    }
+    
     return(
         <div className="admin-dashboard-main">
             <div className="profilecard-btns">
@@ -42,38 +93,7 @@ const AdminDashboard = () => {
                 </div>
             </div>
             <div className="details-for-admin">
-                {
-                    btn === "fd" ?
-                    <div className="fd-history">
-                        <h1 className='heading'> FD History </h1>
-                        <FdHistory
-                            user="Raj"
-                            transaction={"created fd"}
-                            createdAt={new Date()}
-                            amount={5000}
-                            interest={3}
-                            months={6}
-                        />
-                        <FdHistory
-                            user="Preyansh"
-                            transaction={"matured fd"}
-                            createdAt={new Date()}
-                            amount={5000}
-                            interest={2.4}
-                            months={3}
-                        />
-                        <FdHistory
-                            user="Rishi"
-                            transaction={"created fd"}
-                            createdAt={new Date()}
-                            amount={10000}
-                            interest={4.7}
-                            months={18}
-                        />
-                    </div>
-                    :
-                    <CreateRate />
-                }
+                {renderDashboard(btn)}
             </div>
         </div>
     );
---
+++ src/components/Admin/AdminDashboard/CurrentRates.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import './currentrates.css';
+
+const CurrentRates = (props) => {
+
+    const rates = [
+        {
+            interestRate: 4.5,
+            months: 3,
+            for: "student",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 4,
+            months: 3,
+            for: "normal",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 4.6,
+            months: 3,
+            for: "senior",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 5,
+            months: 6,
+            for: "student",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 4.7,
+            months: 6,
+            for: "normal",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 5,
+            months: 6,
+            for: "senior",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 5.5,
+            months: 9,
+            for: "student",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 5.2,
+            months: 9,
+            for: "normal",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 5.6,
+            months: 9,
+            for: "senior",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 6,
+            months: 12,
+            for: "student",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 5.9,
+            months: 12,
+            for: "normal",
+            createdBy: "Pranav"
+        },
+        {
+            interestRate: 6.2,
+            months: 12,
+            for: "senior",
+            createdBy: "Pranav"
+        },
+    ];
+
+    const renderRates = rates.map((rate, id) => {
+        if(rate.for === props.for){
+            return(
+                <tr key={id}>
+                    <td className='duration'>{rate.months} Months</td>
+                    <td>{rate.interestRate}</td>
+                    <td>{rate.createdBy}</td>
+                </tr>
+            );
+        }
+        return null;
+    });
+    
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan={3}>{props.for} {props.for === "senior" ? "Citizens" : ""}</th>
+                </tr>
+                <tr>
+                    <th>Duration</th>
+                    <th>Intereset</th>
+                    <th>Created By Admin</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRates}
+            </tbody>
+        </table>
+    );
+}
+
+export default CurrentRates;
